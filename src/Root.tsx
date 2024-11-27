30916f2137daa0fddae51bf1585fa06725988239
Yurii Kryvorotko <krivorotkokrivorotko2016@gmail.com>
2024-11-27 12:29:26 +0200

    added start pack

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,11 +1,25 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import { App } from './App';
 import { NotFoundPage } from './pages/NotFoundPage';
+import { HomePage } from './pages/HomePage';
+import { CatalogPage } from './pages/CatalogPage';
 
 export const Root = () => (
   <Router>
     <Routes>
       <Route path="/" element={<App />}>
+        <Route path="home" element={<Navigate to="/" replace />} />
+        <Route index element={<HomePage />} />
+        <Route path="catalog">
+          <Route index element={<CatalogPage />} />
+          <Route path=":idMeal" element={<CatalogPage />} />
+        </Route>
+        <Route path="choices" element={<CatalogPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
